6302739f94d694303b9cc620b0a7f9c913b2c8cd
bhavyalikhitha7 <bukka.b@northeastern.edu>
2024-11-30 14:06:42 -0500

    Added routes with createBrowserRouter

---
+++ app/src/routes.ts
@@ -0,0 +1,46 @@
+import { createBrowserRouter } from "react-router";
+import Layout from "./layout"; // New layout component
+import Home from "./pages/HomePage";
+import MarketsPage from "./pages/MarketsPage";
+import CoinPage from "./pages/CoinPage";
+import TradePage from "./pages/TradePage";
+import PortfolioPage from "./pages/PortfolioPage";
+import Signup from "./components/common/Button/Signup";
+import Login from "./components/common/Button/Login";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout, // Use Layout as the root wrapper
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "markets",
+        Component: MarketsPage,
+      },
+      {
+        path: "coins/:id",
+        Component: CoinPage,
+      },
+      {
+        path: "trade/bitcoin",
+        Component: TradePage,
+      },
+      {
+        path: "portfolio",
+        Component: PortfolioPage,
+      },
+      {
+        path: "signup",
+        Component: Signup,
+      },
+      {
+        path: "login",
+        Component: Login,
+      },
+    ],
+  },
+]);
