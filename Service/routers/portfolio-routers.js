c4d8e80541149e6e51e6842d33a262b9d0807b97
bhavyalikhitha7 <bukka.b@northeastern.edu>
2024-12-02 18:56:29 -0500

    added routes for trade and portfolio to fetch api calls

--- Service/routers/portfolio-routers.js
+++ Service/routers/portfolio-routers.js
@@ -22,4 +22,8 @@ router.post('/deposit', PortfolioController.deposit);
 router.post('/withdraw', PortfolioController.withdraw);
 router.post('/update-trading-balance', PortfolioController.updateTradingBalance);
 router.get('/fetch-trading-balance-usd', PortfolioController.fetchTradingBalanceUSD);
+router.post('/add-transaction', PortfolioController.addTransaction);
+router.get('/fetch-transaction-history', PortfolioController.fetchTransactionHistory);
+router.get('/download-transaction-history', PortfolioController.downloadTransactionHistory);
+router.put('/usd-balance-update', PortfolioController.updateTradingBalanceUSD);
 export default router;
