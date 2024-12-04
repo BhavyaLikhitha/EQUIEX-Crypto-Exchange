34bc3f0b949c6f1afd8353a2f84d5410a2ddb95f
bhavyalikhitha7 <bukka.b@northeastern.edu>
2024-12-04 16:24:59 -0500

    Added reducer for portfolio management

--- app/src/store/store.ts
+++ app/src/store/store.ts
@@ -2,12 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import signupReducer from './slices/signupSlice';
 import watchlistReducer from './slices/watchlistSlice';
-
+import portfolioReducer from './slices/portfolioSlice';
 const store = configureStore({
   reducer: {
     auth: authReducer, // Add your authentication slice here
     signup: signupReducer,
     watchlist: watchlistReducer,
+    portfolio: portfolioReducer,
   },
 });
 
