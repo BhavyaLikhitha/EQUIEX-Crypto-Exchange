ff172bb5dc13f29f4a903e1bd285884d040c4b75
bhavyalikhitha7 <bukka.b@northeastern.edu>
2024-12-05 12:32:17 -0500

    minor changes to ui design

--- app/src/components/common/Header/drawer.tsx
+++ app/src/components/common/Header/drawer.tsx
@@ -18,12 +18,12 @@ export default function AnchorTemporaryDrawer(): JSX.Element {
       >
         <div className='drawer-div'>
           <a href='/markets'><p className='link'>Markets</p></a>
-          <a href='/'><p className='link'>Trade</p></a>
-          <a href='/'><p className='link'>NFT Gallery</p></a>
-          <a href='/'><p className='link'>Academy</p></a>
-          <a href='/'><p className='link'>Rewards</p></a>
-          {/* <a href="/"><Login text={"Login"}/></a> */}
-          {/* <a href="/"><Signup text={"Signup"}/></a> */}
+          <a href='/trade/bitcoin'><p className='link'>Trade</p></a>
+          <a href='/nft'><p className='link'>NFT Gallery</p></a>
+          <a href='/blogs'><p className='link'>Academy</p></a>
+          <a href='/rewards'><p className='link'>Rewards</p></a>
+          <a href="/login"><p className='link'>Login</p></a>
+          <a href="/signup"><p className='link'>Signup</p></a>
         </div>
       </Drawer>
     </div>
