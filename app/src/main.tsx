576eb8cd674ad95d5393952d5026b0e830ed1e46
bhavyalikhitha7 <bukka.b@northeastern.edu>
2024-12-04 23:38:49 -0500

    Added internationalization provider to main

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,13 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux';
 import store from './store/store.ts';
-import './i18.ts'; 
-
+import i18n from './i18.ts'; 
+import { I18nextProvider } from 'react-i18next';
 createRoot(document.getElementById('root')!).render(
   
   <StrictMode>
     <Provider store={store}>
+    <I18nextProvider i18n={i18n}>
     <App />
+    </I18nextProvider>
     </Provider>
   </StrictMode>,
 )
