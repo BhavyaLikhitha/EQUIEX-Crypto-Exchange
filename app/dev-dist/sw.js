59388f2d80bcfde0d09448870120792fed99a4f2
bhavyalikhitha7 <bukka.b@northeastern.edu>
2024-12-05 20:54:44 -0500

    modified readme

--- app/dev-dist/sw.js
+++ app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a68f1r8f5jg"
+    "revision": "0.ou7l1oeifn8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
