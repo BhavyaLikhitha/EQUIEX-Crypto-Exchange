fe3e0973770f847b6f168950b503831a3e9db695
bhavyalikhitha7 <bukka.b@northeastern.edu>
2024-12-07 18:06:59 -0500

    Added frontend code documentation

--- app/src/components/coin/selectDays/SelectDays.tsx
+++ app/src/components/coin/selectDays/SelectDays.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import './selectdays.css';
--- app/src/components/common/Footer/index.tsx
+++ app/src/components/common/Footer/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "./footer.css";
 import { useTranslation } from "react-i18next";
 function Footer() {
