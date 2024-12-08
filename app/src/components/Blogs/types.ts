ed328888ac702e98c8a674950bc6f8e7ed3ad1f9
bhavyalikhitha7 <bukka.b@northeastern.edu>
2024-12-07 21:36:02 -0500

    Cleaning main branch before creating pull request from development

--- app/src/components/Blogs/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Blog {
-    id: number;
-    title: string;
-    content: string;
-    author: string;
-    image?: string; // Make image optional if it may not always be present
-  }
