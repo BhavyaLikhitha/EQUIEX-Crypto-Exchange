f1ea9a881aa078f12c16a6b6348f8592369eea37
BhavyaLikhitha <bhavyalikhitha.bbl@gmail.com>
2025-01-10 18:45:27 -0500

    build version

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4t6gujru61"
+    "revision": "0.kundt4k8228"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
