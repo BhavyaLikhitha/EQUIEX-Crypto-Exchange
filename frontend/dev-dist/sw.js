c30991d75e06f2d98dacdf3051370d15b553277c
BhavyaLikhitha <bhavyalikhitha.bbl@gmail.com>
2025-01-10 20:53:53 -0500

    markets page

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1bk6rmufj78"
+    "revision": "0.k2hm2mgv7ag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
